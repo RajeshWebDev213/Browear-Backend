afe270bb0b692b1d48728ce929e99b43a1afde91
RajeshWebDev123 <rajeshpodilapu7032@gmail.com>
2026-02-15 11:57:05 +0530

    email correction

--- Database.js
+++ Database.js
@@ -1,25 +1,26 @@
-const sql = require("mysql2")
-const dotenv = require('dotenv')
-dotenv.config()
-const Database = sql.createConnection({
-    host:process.env.BroWear_DB_Host,
-    user:process.env.BroWear_DB_User,
-    password:process.env.BroWear_DB_Password,
-    database:process.env.BroWear_DB_Database,
-    port:process.env.BroWear_DB_Port
-})
-Database.connect(err=>{
-    if(err){
-        console.log("error is fetched",err)
-    }
-    console.log("Data base connected successfully")
-    Database.query('select * from signupusersData',(err,result)=>{
-         if(err){
-            console.log("The error is : ",err)
-         }
-         console.log("the user details are: ",result)
-         
-         
-    })
-})
-module.exports = {Database}
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const Database = mysql.createPool({
+  host: process.env.BroWear_DB_Host,
+  user: process.env.BroWear_DB_User,
+  password: process.env.BroWear_DB_Password,
+  database: process.env.BroWear_DB_Database,
+  port: process.env.BroWear_DB_Port,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+
+Database.getConnection((err, connection) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+  } else {
+    console.log("Database connected successfully");
+    connection.release();
+  }
+});
+
+module.exports = { Database };
